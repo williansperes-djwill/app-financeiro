d154ee61f1765b409ecb69b19016b8e2edbeb995
Willians Peres <williansperes@gmail.com>
2026-03-10 14:04:33 -0300

    configurando PWA

--- vite.config.js
+++ vite.config.js
@@ -11,37 +11,25 @@ export default defineConfig({
         enabled: true,
       },
       manifest: {
-        {
-  "name": "App Financeiro",
-  "short_name": "Financeiro",
-  "icons": [
-    {
-      "src": "/icon-192.png",
-      "sizes": "192x192",
-      "type": "image/png"
-    },
-    {
-      "src": "/icon-512.png",
-      "sizes": "512x512",
-      "type": "image/png"
-    }
-  ],
-  "theme_color": "#0F172A",
-  "background_color": "#0F172A",
-  "display": "standalone"
-}
+        name: "App Financeiro",
+        short_name: "Financeiro",
+        start_url: ".",
+        display: "standalone",
+        background_color: "#0F172A",
+        theme_color: "#0F172A",
+        icons: [
           {
             src: "/icon-192.png",
             sizes: "192x192",
-            type: "image/png",
+            type: "image/png"
           },
           {
             src: "/icon-512.png",
             sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-  ],
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ]
 });
