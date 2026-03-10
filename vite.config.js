c16c0074bd68036df0ee3283bc24688cdd5320b7
Willians Peres <williansperes@gmail.com>
2026-03-10 12:15:58 -0300

    Atualizando icone profissional

--- vite.config.js
+++ vite.config.js
@@ -11,14 +11,25 @@ export default defineConfig({
         enabled: true,
       },
       manifest: {
-        name: "Financeiro DJ Will",
-        short_name: "Financeiro",
-        description: "Controle financeiro pessoal",
-        theme_color: "#ffffff",
-        background_color: "#ffffff",
-        display: "standalone",
-        start_url: "/",
-        icons: [
+        {
+  "name": "App Financeiro",
+  "short_name": "Financeiro",
+  "icons": [
+    {
+      "src": "/icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ],
+  "theme_color": "#0F172A",
+  "background_color": "#0F172A",
+  "display": "standalone"
+}
           {
             src: "/icon-192.png",
             sizes: "192x192",
